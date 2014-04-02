3f4a6c4f148cfeb8461205ee91fee19a940ae436
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-02 07:36:42 -0700

    Hook up Google Analytics
    Let's see who's visiting the site

--- content/themes/hand-built/functions.php
+++ content/themes/hand-built/functions.php
@@ -26,6 +26,8 @@ class Hand_Built {
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'action_wp_enqueue_scripts' ) );
 
+		add_action( 'wp_footer', array( $this, 'action_wp_footer' ) );
+
 	}
 
 	public function action_wp_enqueue_scripts() {
@@ -35,6 +37,27 @@ class Hand_Built {
 
 		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto+Slab:400,300,700|Open+Sans:400italic,600italic,300,400,600' );
 		wp_enqueue_style( 'hand-built', get_stylesheet_uri() );
+
+	}
+
+	public function action_wp_footer() {
+
+		if ( is_user_logged_in() || ( defined( 'ENVIRONMENT' ) && 'local' === ENVIRONMENT ) ) {
+			return;
+		}
+
+		?>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-49623239-1', 'handbuilt.co');
+			ga('send', 'pageview');
+
+		</script>
+		<?php
 
 	}
 
