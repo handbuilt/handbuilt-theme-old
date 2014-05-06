8f417bf3ad2c08b57cfa19c11aef19b711cb2616
Daniel Bachhuber <d@danielbachhuber.com>
2014-05-06 15:04:40 -0700

    Always use appropriate header meta; link to announcement post

--- content/mu-plugins/environment.php
+++ content/mu-plugins/environment.php
@@ -9,3 +9,11 @@ $environment_defined_theme = function() {
 
 add_filter( 'template', $environment_defined_theme );
 add_filter( 'stylesheet', $environment_defined_theme );
+
+add_filter( 'pre_option_blogname', function() {
+	return 'Hand Built';
+});
+
+add_filter( 'pre_option_blogdescription', function(){
+	return 'Custom WordPress development, code review, data migrations, and consulting services from Daniel Bachhuber.';
+});
